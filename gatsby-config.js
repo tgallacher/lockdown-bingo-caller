b9bca17ef04b065233531591c07e7895fa5770f2
Tom Gallacher <tgallacher@users.noreply.github.com>
2020-05-24 14:47:37 +0100

    chore: update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby + Typescript + Tailwind CSS + Styled-Components Starter`,
-    description: `A bare-bones Typescript + Tailwind CSS + Styled-Components + starter to kickoff your project.`,
-    author: `@reevmich`
+    title: `Lockdown Bingo Caller`,
+    description: `A simple Bingo caller app to help when running your family/friends Bingo night during lockdown.`,
+    author: `@tgallacher`
   },
-  plugins: [`gatsby-plugin-styled-components`, 'gatsby-plugin-typescript']
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-remove-generator',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.NODE_ENV === 'production' && 'UA-72220182-2'
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Lockdown Bingo Caller',
+        short_name: `BingoCaller`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
 };
